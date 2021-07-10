15b21e9d70238a7f77f03c8bf733176bf4726b8f
Masaki Kato <yoru10ji@gmail.com>
2021-07-10 11:21:49 +0900

    fix print test condition

--- app1/tests.py
+++ app1/tests.py
@@ -1,26 +1,6 @@
-import sys
 from django.test import TestCase
-from time import sleep
-
-cow = """
- _____________
-< Test Start! >
- -------------
-        \   ^__^
-         \  (oo)\_______
-            (__)\       )\/\\
-                ||----w |
-                ||     ||
-"""
 
 class SampleTestCase(TestCase):
-    def setUp(self):
-        sleep(0.5)
-
-    def tearDown(self):
-        sleep(0.5)
 
     def test_example(self):
-        print(cow)
-        print("Python{}".format(sys.version))
-
+        self.assertEqual(1+1, 2)
